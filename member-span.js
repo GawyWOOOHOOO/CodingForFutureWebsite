c0017056636c0c81a43d4e3aeed465b6977e6bb0
overlordpro-sys <64398319+overlordpro-sys@users.noreply.github.com>
2023-03-05 13:20:08 -0800

    Finished meet team structure Updated member bios

--- member-span.js
+++ member-span.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
     $(".member-box").click(function(){
-        // $(".inner-our-team").not(this).next(".member-hover").fadeOut();
-        // $(this).next(".member-info").fadeToggle();
+        if($(window).innerWidth() < 992) {
+            $(this).find(".member-info").css({
+                top: 'calc(4px + 100%)',
+                left: '0',
+                width: '100%'
+                })
+        }
+        else
+        {
+            if($(this).offset().left + 1 >= ($(window).width()/2)){
+                $(this).find(".member-info").css({
+                    top: '0',
+                    right: '100%',
+                    left: 'auto',
+                    width: '200%'
+                })
+            }
+            else {
+                $(this).find(".member-info").css({
+                    top: '0',
+                    left: '100%',
+                    right: 'auto',
+                    width: '200%'
+                })
+            }
+        }
         $(".member-box").not(this).find(".member-info").fadeOut();
         $(this).find(".member-info").fadeToggle();
+    })
+
+    $(window).on('resize', function() {
+        $(".member-box").not(this).find(".member-info").hide();
     })
 });
 
